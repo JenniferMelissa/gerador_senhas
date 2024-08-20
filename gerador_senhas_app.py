45ff9246048071e04d8e8e6fde23577b6e740b7a
jennifermel <jennifermelelissa8503@gmail.com>
2024-08-20 15:16:28 -0300

    Gerador de Senhas

--- gerador_senhas_app.py
+++ gerador_senhas_app.py
@@ -4,7 +4,7 @@ while True:
     confirma = input('Gerar senha (s/n)? ').lower()
 
     if confirma == 's':
-        print(secrets.token_urlsafe(16))    #16 é bites que a senha vai ter, é bem segura
+        print(secrets.token_urlsafe(16))    #16 é bits que a senha vai ter (força da senha), é bem segura
         continue
     else:
         print('Programa encerrado.')
